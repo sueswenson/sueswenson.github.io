a578cd489b35d7bd9951a65aff8205d2a3080c4d
sueswenson <sueswenson@Sues-MacBook-Pro.local>
2017-04-03 21:28:21 -0400

    Finished homework

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,35 @@
+/*var numl1 = 10 * 10;
+var numl2 = 100 / 10;
+var numl3 = 50 - 17;
+var numl4 = 50 + 17;
+var myName = 'Sue Swenson  ';
+
+
+document.write(numl1);
+document.write(numl2);
+document.write(numl3);
+document.write(numl4);
+
+document.write(myName);
+
+document.write('hello, world!');
+
+console.log("hello world");*/
+
+document.querySelector('#title').style.color = 'green'
+
+
+
+
+var title = document.querySelector('#title')
+
+
+
+
+
+
+
+
+
+
+
